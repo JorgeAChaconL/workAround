dc2e60bd70882c86f85668d415d0bcdb18e2a391
JorgeAChacon <62924612+JorgeAChaconL@users.noreply.github.com>
2025-02-10 10:21:00 -0600

    Create a new section for each input radio, label for item and raido input

---
+++ main.js
@@ -0,0 +1,44 @@
+import { getRoles, getCompanies } from "./salaryData";
+
+//Get the data from the salaryData.js file
+const companies = getCompanies();
+const roles = getRoles();
+
+//Create Inputs for the companies and roles;
+renderInputButtons(companies, 'company');
+renderInputButtons(roles, 'role');
+
+//Create a new Section with ratio inputs based on data provided in labels arrays;
+function renderInputButtons(labels, groupNane){
+    const container = document.createElement('section');
+    container.setAttribute('id',`${groupNane}Inputs`);
+
+    let header = document.createElement('h3');
+    header.innerText = `Select a ${groupNane};`;
+    container.appendChild(header);
+
+    // Create a label for each item in the array
+    labels.array.forEach(label => {
+        let divElement = document.createElement('div');
+        divElement.setAttribute('class','option');
+
+        let inputElement = document.createElement('input');
+        inputElement.setAttribute('type','radio');
+        inputElement.setAttribute('name', groupNane);
+        inputElement.setAttribute('value', label);
+        divElement.appendChild(inputElement);
+
+
+        //Create label for the radio input
+        let labelElement = document.createElement('lalel');
+        labelElement.setAttribute('for', label);
+        labelElement.innerText = label;
+        divElement.appendChild(labelElement);
+
+        inputElement.addEventListener('click', updateResults);
+
+        container.appendChild(divElement);
+    });    
+
+    document.querySelector('main').appendChild(container);
+}
